c43771627e7fa251e4cbbcd740364349620b5fce
Pierre Thomain <pierre.thomain@bglgroup.co.uk>
2018-02-06 14:14:30 +0000

    Fixing JokeResponse

--- app/src/main/java/uk/co/glass_software/android/cache_interceptor/demo/model/JokeResponse.java
+++ app/src/main/java/uk/co/glass_software/android/cache_interceptor/demo/model/JokeResponse.java
@@ -6,56 +6,57 @@ import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
-import uk.co.glass_software.android.cache_interceptor.response.EmptyResponse;
+import uk.co.glass_software.android.cache_interceptor.interceptors.error.ApiError;
+import uk.co.glass_software.android.cache_interceptor.response.CachedResponse;
 
-public class JokeResponse extends EmptyResponse {
-    
+public class JokeResponse extends CachedResponse<JokeResponse> {
+
     @Override
     public float getTtlInMinutes() {
         return .5f; //30s
     }
-    
+
     @SerializedName("type")
     private String type;
-    
+
     @SerializedName("value")
     private Value value;
-    
+
     public String getType() {
         return type;
     }
-    
+
     public void setType(String type) {
         this.type = type;
     }
-    
+
     public Value getValue() {
         return value;
     }
-    
+
     public void setValue(Value value) {
         this.value = value;
     }
-    
+
     @Override
     public boolean equals(Object o) {
-        
+
         if (this == o) {
             return true;
         }
-        
+
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
-        
+
         JokeResponse joke = (JokeResponse) o;
-        
+
         return new EqualsBuilder()
                 .append(type, joke.type)
                 .append(value, joke.value)
                 .isEquals();
     }
-    
+
     @Override
     public int hashCode() {
         return new HashCodeBuilder(17, 37)
@@ -63,7 +64,7 @@ public class JokeResponse extends EmptyResponse {
                 .append(value)
                 .toHashCode();
     }
-    
+
     @Override
     public String toString() {
         return new ToStringBuilder(this)
