b3ab9e4183dbc3c7f8e98cdb63263f624d6f2e49
Pierre Thomain <pthomain@gmail.com>
2018-10-13 17:19:33 +0100

    Updating copyright

--- cache_interceptor/src/main/java/uk/co/glass_software/android/cache_interceptor/retrofit/RetrofitCacheAdapter.kt
+++ cache_interceptor/src/main/java/uk/co/glass_software/android/cache_interceptor/retrofit/RetrofitCacheAdapter.kt
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2017 Glass Software Ltd
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package uk.co.glass_software.android.cache_interceptor.retrofit
 
 import com.google.gson.Gson
@@ -18,7 +39,6 @@ import java.lang.reflect.Type
 @Suppress("UNCHECKED_CAST")
 internal class RetrofitCacheAdapter<E>(private val rxCacheFactory: RxCacheInterceptor.Factory<E>,
                                        private val logger: Logger,
-                                       private val gson: Gson,
                                        private val methodDescription: String,
                                        private val instruction: CacheInstruction?,
                                        private val rxCallAdapter: CallAdapter<Any, Any>)
--- cache_interceptor/src/main/java/uk/co/glass_software/android/cache_interceptor/retrofit/RetrofitCacheAdapterFactory.kt
+++ cache_interceptor/src/main/java/uk/co/glass_software/android/cache_interceptor/retrofit/RetrofitCacheAdapterFactory.kt
@@ -1,6 +1,26 @@
+/*
+ * Copyright (C) 2017 Glass Software Ltd
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package uk.co.glass_software.android.cache_interceptor.retrofit
 
-import com.google.gson.Gson
 import com.google.gson.internal.`$Gson$Types`.getRawType
 import io.reactivex.Completable
 import io.reactivex.Observable
@@ -20,8 +40,7 @@ import java.lang.reflect.Type
 class RetrofitCacheAdapterFactory<E> internal constructor(private val rxJava2CallAdapterFactory: RxJava2CallAdapterFactory,
                                                           private val rxCacheFactory: RxCacheInterceptor.Factory<E>,
                                                           private val annotationProcessor: AnnotationProcessor<E>,
-                                                          private val logger: Logger,
-                                                          private val gson: Gson)
+                                                          private val logger: Logger)
     : CallAdapter.Factory()
         where E : Exception,
               E : NetworkErrorProvider {
@@ -73,7 +92,6 @@ class RetrofitCacheAdapterFactory<E> internal constructor(private val rxJava2Cal
                 RetrofitCacheAdapter(
                         rxCacheFactory,
                         logger,
-                        gson,
                         methodDescription,
                         instruction,
                         callAdapter
